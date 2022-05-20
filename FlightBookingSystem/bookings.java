877d2eb5b75c7dd220d0f5350c87ac11815c3142
Danielle Higgins <higdan4024@gmail.com>
2022-05-19 21:45:35 -0400

    changes

--- FlightBookingSystem/bookings.java
+++ FlightBookingSystem/bookings.java
@@ -1,5 +1,16 @@
 package FlightBookingSystem;
 
 public class bookings {
-    
+    // Declaring private variables
+    private UUID uuid;
+    private String userID;
+    private String flightID;
+    private String hotelID;
+    private int seatNumber;
+    private int roomNumber;
+    private String dateOfBooking;
+    private String bookingName;
+    private String bookingDateOfBirth;
+    private String hotelCheckin;
+    private String hotelCheckout;
 }
